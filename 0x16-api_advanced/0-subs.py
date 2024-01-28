f33620265bb05290100f0121a7c4f9c4aba7362a
MuhammedAbdelnaser <mohamedelreety2014@gmail.com>
2024-01-28 19:00:21 +0200

    fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,22 +1,17 @@
 #!/usr/bin/env python3
 """ a Python script that, using this REST API, returns the number of subscribers subreddit. """
+
 import requests
-import sys
 
 def number_of_subscribers(subreddit):
-
   subreddit_url = f"https://www.reddit.com/r/{subreddit}/about.json"
 
-  headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+  headers = {'User-Agent': 'Mozilla/5.0'}
 
-  try:
-    response = requests.get(subreddit_url, headers=headers)
-    data = response.json()
-    subscribers = data["data"]["subscribers"]
-    return subscribers
-  except KeyError:
-    return 0
-  except requests.exceptions.RequestException as e:
-    print("Error:", e)
+  response = requests.get(subreddit_url, headers=headers)
+
+  if response.status_code != 200:
     return 0
 
+  data = response.json()
+  return data.get("data", {}).get("subscribers", 0)
