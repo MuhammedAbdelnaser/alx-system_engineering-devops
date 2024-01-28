1d69bde64fbb2bd6dfdfdecfa389830164316a17
MuhammedAbdelnaser <mohamedelreety2014@gmail.com>
2024-01-28 19:30:58 +0200

    damn it

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,27 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
+"""Query Reddit API for subreddit subscriber count"""
+
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "0x16-api_advanced:project: v1.0.0"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
-        return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+  """
+    Queries the Reddit API to retrieve the subscriber count of a subreddit.
+
+    Args:
+      subreddit (str): The name of the subreddit.
+
+    Returns:
+      int: The number of subscribers of the specified subreddit.
+           Returns 0 if the subreddit is invalid or inaccessible.
+    """
+  subreddit_url = f"https://www.reddit.com/r/{subreddit}/about.json"
+
+  headers = {'User-Agent': 'Mozilla/5.0'}
+
+  response = requests.get(subreddit_url, headers=headers)
+
+  if response.status_code != 200:
+    return 0
+
+  data = response.json()
+  return data.get("data", {}).get("subscribers", 0)
