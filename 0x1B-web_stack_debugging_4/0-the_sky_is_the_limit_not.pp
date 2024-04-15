d92ba1422dd5215756399140d39a2af8353d098d
MuhammedAbdelnaser <mohamedelreety2014@gmail.com>
2024-04-15 20:33:55 +0200

    enhancement

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,4 @@
-# web debug 4
+# web stack debugging 4
 
 exec { 'debug4':
   command  => 'sudo apt-get purge nginx nginx-common nginx-full -y; sudo apt-get install nginx -y; sudo service nginx restart',
